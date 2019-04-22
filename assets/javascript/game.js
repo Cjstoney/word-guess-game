104c901bac68aa9e8b8db9abf1cfc579fe43570a
Camerone Stoney <Cameronestoney@yahoo.com>
2019-04-22 11:30:10 -0700

    additiontal html and a start on the js

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// name list
+var selectableNames = ["bellatrix", "harry", "dumbledore", 
+    "voldemort", "dobby", "ginny", "remus", "snape", "luna", "sirius"];
+
+// max number of tries
+var letterGuessed;
+
+var remainingChances = 0;
+
+var maxchances;
+
+var guessingIndex =[];
